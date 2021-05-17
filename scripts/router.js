c37dbaba67215a5b7254bc719674034c97cd7d19
Tony Guan <t9guan@ucsd.edu>
2021-05-17 05:08:42 -0700

    added sc

--- scripts/router.js
+++ scripts/router.js
@@ -37,7 +37,7 @@ router.setState = function(state, num) {
    */
 
   if(state == 'home'){
-    history.pushState({'page_id': 1}, document.title, '');
+    history.pushState({'page_id': 0}, document.title, '#');
     //history.back();
   }else if(state == 'settings'){
     history.pushState({'page_id': 0}, document.title, '#settings');
--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,21 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
     .then(entries => {
-      var i = 1;
+      let i = 1;
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
         document.querySelector('main').appendChild(newPost);
         newPost.addEventListener('click', () => {
           console.log('clicked');
+          
           setState(newPost,i);
-          i++;
           document.body.classList.add('single-entry');
           document.querySelector('h1').innerHTML = 'Entry ' + i;
           document.querySelector('entry-page').remove();
           document.querySelector('body').appendChild(document.createElement('entry-page'));
           document.querySelector('entry-page').entry = entry;
+          i++;
         });
       });
     });
